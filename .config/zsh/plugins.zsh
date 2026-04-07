91b8cf8ad0322a3858b5fb3c64c44f988a2a3b04
c0mplex <sadasipa@gmail.com>
2026-04-07 11:52:31 -0500

    Breaking changes

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -2,66 +2,10 @@
 ## Plugins
 ##
 
-# Zinit's installer
-#ZINIT_HOME="${ZINIT_HOME:-${XDG_DATA_HOME:-${HOME}/.local/share}/zinit}"
-
-#if [[ ! -f ${ZINIT_HOME}/zinit.git/zinit.zsh ]]; then
-    #print -P "%F{14}▓▒░ Installing Flexible and fast ZSH plugin manager %F{13}(zinit)%f"
-    #command mkdir -p "${ZINIT_HOME}" && command chmod g-rwX "${ZINIT_HOME}"
-    #command git clone https://github.com/zdharma-continuum/zinit.git "${ZINIT_HOME}/zinit.git" && \
-        #print -P "%F{10}▓▒░ Installation successful.%f%b" || \
-        #print -P "%F{9}▓▒░ The clone has failed.%f%b"
-#fi
-
-#source "${ZINIT_HOME}/zinit.git/zinit.zsh"
 source /usr/share/fzf/completion.zsh
 source /usr/share/fzf/key-bindings.zsh
 
 eval "$(sheldon source)"
-#autoload -Uz compinit
-#if [[ -n ${HOME}/.zcompdump(#qN.m-1) ]]; then
-  #compinit -C -Q
-#else
-  #compinit -Q
-#fi
-#zinit cdreplay -q
-
-# ZSH-VI
-#zinit ice wait"0" lucid
-#zinit light jeffreytse/zsh-vi-mode
-
-#zinit ice wait"0" lucid blockf atpull'zinit creinstall -q .'
-#zinit light zsh-users/zsh-completions
-
-#zinit ice wait"1" 
-#zinit light hlissner/zsh-autopair 
-
-#zinit ice wait"0" lucid
-#zinit light-mode for \
-  #zsh-users/zsh-autosuggestions \
-  #Aloxaf/fzf-tab
-
-#zinit ice wait"1" lucid
-#zinit light zsh-users/zsh-history-substring-search \
-
-#zdharma-continuum/history-search-multi-word
-
-# FZF
-#zinit ice from"gh-r" as"command"
-#zinit light junegunn/fzf-bin
-
-# EZA
-#zinit ice wait lucid from"gh-r" as"program" 
-#zinit light eza-community/eza
-
-# BAT
-#zinit ice wait lucid from"gh-r" as"program" mv"*/bat -> bat" atload"export BAT_THEME='Nord'"
-#zinit light sharkdp/bat
-
-#load syntax highlight at last to avoid race conditions
-
-#zinit ice wait"2" lucid atinit"zpcompinit; zpcdreplay"
-#zinit light zdharma-continuum/fast-syntax-highlighting
 
 function man() {
   LESS_TERMCAP_mb=$'\e[1;31m' \
--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -3,14 +3,17 @@
 ##
 
 eval "$(starship init zsh)"
-#PROMPT2="                    ❯"
-precmd() {
-    local prompt_rendered="$(starship prompt)"
-    local clean=$(print -P "$prompt_rendered" | sed 's/\x1b\[[0-9;]*m//g')
-    PROMPT_INDENT_WIDTH=${#clean}
-    PROMPT2=$(printf "%*s\e[1m❯\e[0m " $(($PROMPT_INDENT_WIDTH - 2)) "")
+autoload -Uz add-zsh-hook
+continuation_line() {
+      local prompt_rendered="$(starship prompt)"
+      local clean=$(print -P "$prompt_rendered" | sed 's/\x1b\[[0-9;]*m//g')
+      PROMPT_INDENT_WIDTH=${#clean}
+      PROMPT2=$(printf "%*s%%{\e[1;38;2;251;241;199m%%}❯%%{\e[0m%%} " $(($PROMPT_INDENT_WIDTH - 2)) "")
 }
-/home/c0mplex/.scripts/system/birthday 
+add-zsh-hook precmd continuation_line
+continuation_line
+
+/home/c0mplex/.scripts/system/birthday
 /home/c0mplex/.scripts/loginart/optimizedlogincatwithhtext
 
 # vim:ft=zsh
