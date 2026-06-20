c587a1f9ede501eccdd51bdb01638ff3fcdb3fca
c0mplex <sadasipa@gmail.com>
2026-06-20 09:27:01 -0500

    using /home/c0mplex instead of hardcode

--- .scripts/update.sh
+++ .scripts/update.sh
@@ -1,3 +1,3 @@
 #/usr/bin/env sh
-# /home/c0mplex/dotfiles/.files/suckless/backup/st/st -n floating -e sh
-/home/c0mplex/dotfiles/.files/suckless/alternatives/stliga -n floating -f "Fira Code:size=12" -e sh -c "sudo pacman -Syu"
+# $HOME/dotfiles/.files/suckless/backup/st/st -n floating -e sh
+$HOME/dotfiles/.files/suckless/alternatives/stliga -n floating -f "Fira Code:size=12" -e sh -c "sudo pacman -Syu"
