1bfc5210397260dddab2ee25f2377a440d15c759
c0mplex <sadasipa@gmail.com>
2026-06-19 22:57:37 -0500

    gitaliases modification

--- .config/zsh/unsourced/gitaliases.zsh
+++ .config/zsh/unsourced/gitaliases.zsh
@@ -11,12 +11,6 @@ _git_log_prettily() {
   git log --pretty="$1" "${@:2}"
 }
 
-alias gc='git commit'
-alias gca='git commit -a'
-alias gco='git checkout'
-alias gcb='git copy-branch-name'
-alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias gac='git add -A && git commit -m'
 alias ge='git-edit-new'
 alias commit="git add . && git commit -m"
